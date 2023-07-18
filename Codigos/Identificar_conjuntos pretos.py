c2be6e2a275c9055e0c3ecaf64d91fe206e542e5
GabrielMaurina <gabrielmaurina2003@gmail.com>
2023-07-18 10:42:03 -0300

    ajusta repositorio

---
+++ Codigos/Identificar_conjuntos pretos.py
@@ -0,0 +1,82 @@
+import cv2
+
+def lineanize_black_hsv(frame):    
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    black_mask = cv2.inRange(hsv, (0, 20, 0), (255, 255, 90))
+    black_result= cv2.bitwise_and(frame, frame, mask=black_mask)
+    
+    gray = cv2.cvtColor( black_result, cv2.COLOR_BGR2GRAY)
+    _,gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)   
+    cv2.imshow('Imagem binaria por hsv', gray)
+    
+    return gray
+# def lineanize_black_gray(frame):
+#     gray = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY)
+#     _,gray = cv2.threshold(gray, 30, 255, cv2.THRESH_BINARY_INV)
+#     cv2.imshow('Imagem binaria por gray', gray)
+    
+#def lineanize_black_bgr(frame): 
+#    # marcador pra saber se o pixel pertence ao intervalo ou não   
+#    black_mask = cv2.inRange(frame, (0, 0, 0), (250, 50, 50))
+#    cv2.imshow("MASCARA",black_mask)
+#    # aplica máscara que "deixa passar" pixels pertencentes ao intervalo, como filtro
+#    black_result= cv2.bitwise_and(frame, frame, mask=black_mask)
+#    cv2.imshow("FRAME - MASCARA",black_result)
+#    #aplica linearização
+#    gray = cv2.cvtColor( black_result, cv2.COLOR_BGR2GRAY)
+#    _, gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+#    cv2.imshow("BINARIO",gray)
+#    return gray
+    
+def trancking(frame, gray):
+    #encontra pontos que circundam regiões conexas (contour)
+    contours, hierarchy = cv2.findContours(gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    #se existir contornos    
+    if contours:
+        #retornando a área do primeiro grupo de pixels brancos
+        max_Area = cv2.contourArea(contours[0])
+        contour_Max_Area_Id = 0
+        i = 0
+        #para cada grupo de pixels branco existente
+        for cnt in contours:
+            #procura o grupo com a maior área
+            if max_Area < cv2.contourArea(cnt):
+                max_Area = cv2.contourArea(cnt)
+                contour_Max_Area_Id = i
+            i += 1
+            
+        #achei o contorno com maior área em pixels
+        cntMaxArea = contours[contour_Max_Area_Id]
+        #retorna um retângulo que envolve o contorno em questão
+        x_Rect, y_Rect, w_Rect, h_Rect = cv2.boundingRect(cntMaxArea)
+        #desenha caixa envolvente com espessura 3
+        cv2.rectangle(frame, (x_Rect, y_Rect), (x_Rect + w_Rect, y_Rect + h_Rect), (0, 0, 255), 2)
+        cv2.circle(frame, (x_Rect + int(w_Rect/2), y_Rect + int(h_Rect/2)), 5, (0, 255, 0), -1)
+        
+    return frame
+    
+def main():
+    webcam = cv2.VideoCapture(0)
+    
+    while True:
+        validacao, frame = webcam.read()
+    
+        if validacao:           
+            
+            gray = lineanize_black_hsv(frame)
+            # lineanize_black_bgr(frame)
+            # gray = lineanize_black_bgr(frame)
+            frame = trancking(frame, gray)
+            
+            cv2.imshow('Pincipal', frame)            
+            
+            if cv2.waitKey(1) != -1:
+                break
+        else:
+            break    
+    webcam.release()
+    cv2.destroyAllWindows()
+
+    
+if __name__ == '__main__':
+    main()
