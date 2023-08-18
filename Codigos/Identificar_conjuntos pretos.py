9bbc69dbf475a81ec37ea76280d188a8a0981111
GabrielMaurina <gabrielmaurina2003@gmail.com>
2023-08-17 21:15:43 -0300

    ajustes no codigo pos_ferias, (relembrar)

--- Codigos/Identificar_conjuntos pretos.py
+++ Codigos/Identificar_conjuntos pretos.py
@@ -1,36 +1,49 @@
+import time
 import cv2
+import numpy as np
 
+# teste de rpg pra hsv depois linariza
 def lineanize_black_hsv(frame):    
+    # transforma a imagem de bgr em hsv
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # marcador pra saber se o pixel pertence ao intervalo ou não   
     black_mask = cv2.inRange(hsv, (0, 20, 0), (255, 255, 90))
+    # aplica máscara que "deixa passar" pixels pertencentes ao intervalo, como filtro
     black_result= cv2.bitwise_and(frame, frame, mask=black_mask)
-    
-    gray = cv2.cvtColor( black_result, cv2.COLOR_BGR2GRAY)
-    _,gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)   
-    cv2.imshow('Imagem binaria por hsv', gray)
-    
-    return gray
-# def lineanize_black_gray(frame):
-#     gray = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY)
-#     _,gray = cv2.threshold(gray, 30, 255, cv2.THRESH_BINARY_INV)
-#     cv2.imshow('Imagem binaria por gray', gray)
-    
+    #lineariza agora para tons de cinza    
+    binary_frame = cv2.cvtColor( black_result, cv2.COLOR_BGR2GRAY)
+    _,binary_frame = cv2.threshold(binary_frame, 0, 255, cv2.THRESH_BINARY)   
+    cv2.imshow('Imagem binaria por hsv', binary_frame)
+    # tentar diminuir o ruido aomentando os espaços do pixel brancos
+    pixel_cover = np.ones((2, 2), np.uint8)
+    binary_frame = cv2.dilate(binary_frame, pixel_cover, iterations=10)
+    cv2.imshow('Imagem com menos ruido', binary_frame)
+
+    return binary_frame
+
+##teste de rpg pra cinza depois lineariza
+#def lineanize_black_gray(frame):
+#    gray = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY)
+#    _,gray = cv2.threshold(gray, 30, 255, cv2.THRESH_BINARY_INV)
+#    cv2.imshow('Imagem binaria por cinza', gray)
+#    return gray
+
+## teste usaando mascara rpg    
 #def lineanize_black_bgr(frame): 
-#    # marcador pra saber se o pixel pertence ao intervalo ou não   
 #    black_mask = cv2.inRange(frame, (0, 0, 0), (250, 50, 50))
-#    cv2.imshow("MASCARA",black_mask)
-#    # aplica máscara que "deixa passar" pixels pertencentes ao intervalo, como filtro
+#    # cv2.imshow("MASCARA",black_mask)
 #    black_result= cv2.bitwise_and(frame, frame, mask=black_mask)
 #    cv2.imshow("FRAME - MASCARA",black_result)
-#    #aplica linearização
+#    # aplica linearização
 #    gray = cv2.cvtColor( black_result, cv2.COLOR_BGR2GRAY)
 #    _, gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
-#    cv2.imshow("BINARIO",gray)
+#    cv2.imshow('Imagem binaria por gbr', gray)
 #    return gray
     
-def trancking(frame, gray):
+    
+def trancking(frame, hsv):
     #encontra pontos que circundam regiões conexas (contour)
-    contours, hierarchy = cv2.findContours(gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(hsv, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     #se existir contornos    
     if contours:
         #retornando a área do primeiro grupo de pixels brancos
@@ -53,6 +66,12 @@ def trancking(frame, gray):
         cv2.rectangle(frame, (x_Rect, y_Rect), (x_Rect + w_Rect, y_Rect + h_Rect), (0, 0, 255), 2)
         cv2.circle(frame, (x_Rect + int(w_Rect/2), y_Rect + int(h_Rect/2)), 5, (0, 255, 0), -1)
         
+        #desenha o ponto no meio da camera
+        height, width, _ = frame.shape
+
+        cv2.circle(frame, (int(width/2), int(height/2)), 5, (255, 0, 0), -1)
+        #desenha o vetor do ponto centra da tela a ate o ponto central da regiao detectada        
+        frame = cv2.line(frame, (int(width/2), int(height/2)), (x_Rect + int(w_Rect/2), y_Rect + int(h_Rect/2)), (0, 255, 0), 2)
     return frame
     
 def main():
@@ -63,12 +82,17 @@ def main():
     
         if validacao:           
             
-            gray = lineanize_black_hsv(frame)
-            # lineanize_black_bgr(frame)
-            # gray = lineanize_black_bgr(frame)
-            frame = trancking(frame, gray)
-            
-            cv2.imshow('Pincipal', frame)            
+            hsv = lineanize_black_hsv(frame)
+            #bgr = lineanize_black_bgr(frame)
+            #gray =  lineanize_black_gray(frame)
+
+            frame = trancking(frame, hsv)
+            cv2.imshow('HSV', frame)            
+            time.sleep(0.01)
+            #frame2 = trancking(frame, bgr)
+            #cv2.imshow('BGR', frame2)            
+            #frame3 = trancking(frame, gray)
+            #cv2.imshow('GRAY', frame3)            
             
             if cv2.waitKey(1) != -1:
                 break
