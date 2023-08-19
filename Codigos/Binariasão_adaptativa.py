fa12cbf6ccb493ad1191d1050ca87eba97daf924
GabrielMaurina <gabrielmaurina2003@gmail.com>
2023-08-19 00:05:41 -0300

    ajuste preeenchimento interno da imagem

--- Codigos/Binariasão_adaptativa.py
+++ Codigos/Binariasão_adaptativa.py
@@ -19,8 +19,8 @@ import numpy as np
 #    _,frame = cv2.threshold(frame, 0, 255, cv2.THRESH_BINARY)  
 #    cv2.imshow('HSV binario', frame)
 #    
-#    pixel_cover = np.ones((2, 2), np.uint8)
-#    frame = cv2.dilate(frame, pixel_cover, iterations=1)
+#    kernel = np.ones((2, 2), np.uint8)
+#    frame = cv2.dilate(frame, kernel, iterations=1)
 #    cv2.imshow('com menos ruido', frame)
 #
 #    return frame
@@ -30,19 +30,24 @@ def lineanize_hsv_adaptado(frame):
     black_mask = cv2.inRange(hsv, (0, 0, 0), (255, 50, 50))
     black_result= cv2.bitwise_and(frame, frame, mask=black_mask)
     frame = cv2.cvtColor( black_result, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Binario', frame)
     # dois ultimos parametros que ajustão
     # O valor limite é a média da área da vizinhança menos a constante 
     # frame = cv2.adaptiveThreshold(frame,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,15,0)
     # O valor limite é uma soma ponderada gaussiana dos valores da vizinhança menos a constante C
-    frame = cv2.adaptiveThreshold(frame,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,5,0)
-    cv2.imshow('HSV binario adapatado', frame)
+    frame = cv2.adaptiveThreshold(frame,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,15,0)
+    cv2.imshow('binario adapatado', frame)
+    # Otsu determina um valor limite global ideal a partir do histograma da imagem.
+    _, frame = cv2.threshold(frame, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    cv2.imshow('binario + otsu', frame)
     
-    pixel_cover = np.ones((2, 2), np.uint8)
-    frame = cv2.dilate(frame, pixel_cover, iterations=2)
-    cv2.imshow('com menos ruido', frame)
+    kernel = np.ones((4, 4), np.uint8)
+    #preenchimento por dilatação de pixel
+    #frame = cv2.dilate(frame, kernel, iterations=2)
+    frame = cv2.morphologyEx(frame, cv2.MORPH_CLOSE, kernel)
+    cv2.imshow('binario com menos ruido', frame)
 
-    return frame
-    
+    return frame    
     
 def trancking(frame, hsv):
     contours, hierarchy = cv2.findContours(hsv, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -101,6 +106,5 @@ def main():
     webcam.release()
     cv2.destroyAllWindows()
 
-    
 if __name__ == '__main__':
     main()
